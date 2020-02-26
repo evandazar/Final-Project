155ea9e12c3aae3dd4e6ccfcbe853101743751a7
evandazar <eval2380@giant.cos.edu>
2020-02-26 15:55:53 -0800

    added click handlers and win arrays.

---
+++ script.js
@@ -0,0 +1,27 @@
+const winSets = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
+    [2,4,6],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8]
+];
+
+function handleClick(clickedSquareEvent)
+{
+    const squareClicked = clickedSquareEvent.target;
+    const squareClickedId = parseInt(
+    squareClicked.getAttribute("id")
+    );
+}
+
+
+/*const grid = () => Array.from(document.getElementsByClassName("game-square"));
+const clickFun = (event) => console.log(event.target);
+
+const listenersEnabled = () => grid().forEach(gsEl => gsEl.addEventListener("click", clickFun));
+const listenersDisabled = () => grid().forEach(gsEl => gsEl.removeEventListener("click", clickFun));
+
+listenersEnabled();*/
