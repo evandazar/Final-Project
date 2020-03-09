b0a7a2e11b28b172fe88a6d0f3fa6d6c39c87b5b
evandazar <eval2380@giant.cos.edu>
2020-03-09 13:47:37 -0700

    redid the click events.

--- script.js
+++ script.js
@@ -8,25 +8,101 @@ const winSets = [
     [1,4,7],
     [2,5,8]
 ];
-
 let currentPlayer = "X";
 
-document.querySelectorAll('.game-square').forEach(square => square.addEventListener('click', handleClick));
+let squareGet = document.getElementById("game-square");
+
+let currentGame = ["", "", "", "", "", "", "", "", ""];
+
+let GameOn = true;
+
+const winMessage = () => currentPlayer + " has won";
+
+const catsGame = () => "Cats Game";
+
+//const squareId = (square) => Number.parseInt(square.id.replace("square-", ""));
+
 
 function handleClick(clickedSquareEvent)
 {
     const squareClicked = clickedSquareEvent.target;
-    const squareClickedId = parseInt(
-    squareClicked.getAttribute("id")
+    squareClicked.innerText = currentPlayer;
+    const squareClickedId = Number.parseInt(
+    squareClicked.id.replace("square-", "")
     );
-    console.log(clickedSquareEvent.target);
+    if (currentGame[squareClickedId] != "" || !GameOn)
+    {
+        return;
+    }
+    console.log(squareClickedId);
+    handleCurrentPlayer(squareClicked, squareClickedId);
+    handleWinCon();
 }
 
 
-/*const grid = () => Array.from(document.getElementsByClassName("game-square"));
-const clickFun = (event) => console.log(event.target);
+document.querySelectorAll('.game-square').forEach(square => square.addEventListener('click', handleClick));
 
-const listenersEnabled = () => grid().forEach(gsEl => gsEl.addEventListener("click", clickFun));
-const listenersDisabled = () => grid().forEach(gsEl => gsEl.removeEventListener("click", clickFun));
+//const listenersEnabled = () => grid().forEach(square => square.addEventListener("click", handleClick));
 
-listenersEnabled();*/
+//const grid = () => Array.from(document.getElementsByClassName("game-square"));
+
+//document.querySelectorAll("game-square").forEach(square => square.addEventListener("click", handleClick));
+
+/*
+const listenersDisabled = () => grid().forEach(square => square.removeEventListener("click", clickFun));
+
+const olistenersEnabled = () => grid().forEach(square => square.addEventListener("click", clickFunO));
+const olistenersDisabled = () => grid().forEach(square => square.removeEventListener("click", clickFunO));
+
+
+
+const emptySquare = () => grid().filter(square => square.innerText == "");
+
+const sameCheck = (arr) => arr.every(square => square.innerText == arr[0].innerText && square.innerText != "");
+
+const yourMove = (index, letter) => grid()[index].innerText = letter;
+
+
+const clickFun = (event) => {
+    listenersEnabled();
+    olistenersDisabled();
+    console.log(event.target.id);
+    console.log(event.target.innerText);
+    if (event.target.innerText == 'x' || 'o')
+    {
+    //listenersDisabled();
+    yourMove(squareId(event.target), "x");
+    }
+    else
+    {
+        //yourMove(squareId(event.target), "x");
+        listenersDisabled();
+    }
+    //clickFunO();
+}
+
+const clickFunO = (event) => {
+    olistenersEnabled();
+    listenersDisabled();
+    console.log(event.target.id);
+    //yourMove(squareId(event.target), "o");
+    if (event.target.innerText = 'x' || 'o')
+    {
+    olistenersDisabled();
+    //yourMove(squareId(event.target), "x");
+    }
+    else
+    {
+        yourMove(squareId(event.target), "o");
+    }
+    clickFun();
+}*/
+
+
+
+//listenersEnabled();
+
+/*
+
+
+*/
